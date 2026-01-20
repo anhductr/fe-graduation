a9658069593a9d47c429e9b70b89403276eb8d59
anhductr <117356056+anhductr@users.noreply.github.com>
2026-01-20 19:08:19 +0700

    Fix sidebar and category list UI

--- admin/admin ecom/src/components/layout/SideBar.jsx
+++ admin/admin ecom/src/components/layout/SideBar.jsx
@@ -13,6 +13,7 @@ import { LiaClipboardListSolid } from "react-icons/lia";
 import { BsChatRightQuote } from "react-icons/bs";
 import { MdRateReview } from "react-icons/md";
 import { LuMessageCircle } from "react-icons/lu";
+import { GoGraph } from "react-icons/go";
 
 
 export default function SideBar({ location }) {
@@ -25,8 +26,8 @@ export default function SideBar({ location }) {
         if (path.startsWith("/categories")) return 5;
         if (path.startsWith("/orders")) return 6;
         if (path.startsWith("/promotion")) return 7;
-        if (path.startsWith("/reviews")) return 9; // Changed from 9 and 10
-        if (path.startsWith("/contents")) return 8;
+        if (path.startsWith("/reviews")) return 8; // Changed from 9 and 10
+        if (path.startsWith("/contents")) return 9;
         if (path.startsWith("/analysis")) return 10;
         if (path.startsWith("/settings")) return 11;
         return 1; // fallback
@@ -193,7 +194,7 @@ export default function SideBar({ location }) {
                             <div
                                 className="w-full text-white !text-left !flex !justify-between items-center !py-[12px] !px-[15px] gap-2 !h-[80px]"
                             >
-                                <Button variant="text" className={`${active === 9 ? "!bg-gradient-to-r !from-[#4a2fcf] !to-[#6440F5] !shadow !border-0 " : ""} !justify-start !text-left !text-white !flex !w-[90%] !items-center !h-full !p-3 !py-[35px] !rounded-[10px] gap-1`}>
+                                <Button variant="text" className={`${active === 8 ? "!bg-gradient-to-r !from-[#4a2fcf] !to-[#6440F5] !shadow !border-0 " : ""} !justify-start !text-left !text-white !flex !w-[90%] !items-center !h-full !p-3 !py-[35px] !rounded-[10px] gap-1`}>
                                     <span aria-label='icon'>
                                         <MdRateReview className={`!text-[37px] !mr-[10px]`} />
                                     </span>
@@ -212,7 +213,7 @@ export default function SideBar({ location }) {
                             <div
                                 className="w-full text-white !text-left !flex !justify-between items-center !py-[12px] !px-[15px] gap-2 !h-[80px]"
                             >
-                                <Button variant="text" className={`${active === 8 ? "!bg-gradient-to-r !from-[#4a2fcf] !to-[#6440F5] !shadow !border-0 " : ""} !justify-start !text-left !text-white !flex !w-[90%] !items-center !h-full !p-3 !py-[35px] !rounded-[10px] gap-1`}>
+                                <Button variant="text" className={`${active === 9 ? "!bg-gradient-to-r !from-[#4a2fcf] !to-[#6440F5] !shadow !border-0 " : ""} !justify-start !text-left !text-white !flex !w-[90%] !items-center !h-full !p-3 !py-[35px] !rounded-[10px] gap-1`}>
                                     <span aria-label='icon'>
                                         <BsChatRightQuote className={`!text-[37px] !mr-[10px]`} />
                                     </span>
@@ -230,9 +231,9 @@ export default function SideBar({ location }) {
                             <div
                                 className="w-full text-white !text-left !flex !justify-between items-center !py-[12px] !px-[15px] gap-2 !h-[80px]"
                             >
-                                <Button variant="text" className={`${active === 8 ? "!bg-gradient-to-r !from-[#4a2fcf] !to-[#6440F5] !shadow !border-0 " : ""} !justify-start !text-white !flex !w-[90%] !items-center !h-full !p-3 !py-[35px] !rounded-[10px] gap-1`}>
+                                <Button variant="text" className={`${active === 10 ? "!bg-gradient-to-r !from-[#4a2fcf] !to-[#6440F5] !shadow !border-0 " : ""} !justify-start !text-white !flex !w-[90%] !items-center !h-full !p-3 !py-[35px] !rounded-[10px] gap-1`}>
                                     <span aria-label='icon'>
-                                        <BsChatRightQuote className={`!text-[37px] !mr-[10px]`} />
+                                        <GoGraph className={`!text-[37px] !mr-[10px]`} />
                                     </span>
                                     <div className='flex flex-col items-start'>
                                         <span className="!font-semibold !text-[14px]">Thống kê</span>
